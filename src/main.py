5e09e1cdfa0a06e8cae0c878c39c297cd770583a
Bjarne <47014074+iK4oS@users.noreply.github.com>
2024-02-01 16:19:05 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,14 +3,28 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-bot = discord.Bot()
+bot = discord.Bot(debug_guilds=[int(os.getenv('DEBUG_GUILD'))], intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name = "hello", description = "Say hello to the bot")
-async def hello(ctx):
-    await ctx.respond("Hey!")
+def load_extension(exceptions: list = []):
+    ''' Loads all extensions from the extensions/ folder. '''
+    CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-bot.run(os.getenv('TOKEN'))
+    for file_name in os.listdir(f'{CURRENT_PATH}/extensions'):
+        cog_name = file_name[:-3]
+        if not file_name.endswith('.py') or cog_name in exceptions:
+            continue
+
+        try:
+            bot.load_extension(f'extensions.{cog_name}')
+            print(f'Module loaded: {cog_name}')
+
+        except Exception as e:
+            print(f'{type(e).__name__}: {e}')
+
+if __name__ == '__main__':
+    load_extension()
+    bot.run(os.getenv('TOKEN'))
