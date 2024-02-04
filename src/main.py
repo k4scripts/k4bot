0027714b1dbf75d22bfcfbea49a85463b2eaaea8
Bjarne <47014074+iK4oS@users.noreply.github.com>
2024-02-04 23:58:26 +0100

    Renamed extensions to commands

--- src/main.py
+++ src/main.py
@@ -13,23 +13,23 @@ async def on_ready():
     logger.info(f"{bot.user} is ready and online!")
 
 
-def load_extension(exceptions: list = []):
-    ''' Loads all extensions from the extensions/ folder. '''
+def load_commands(exceptions: list = []):
+    ''' Loads all commands from the commands/ folder. '''
     CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-    for file_name in os.listdir(f'{CURRENT_PATH}/extensions'):
+    for file_name in os.listdir(f'{CURRENT_PATH}/commands'):
         cog_name = file_name[:-3]
         if not file_name.endswith('.py') or cog_name in exceptions:
             continue
 
         try:
-            bot.load_extension(f'extensions.{cog_name}')
-            logger.info(f'Module loaded: {cog_name}')
+            bot.load_extension(f'commands.{cog_name}')
+            logger.info(f'Command loaded: {cog_name}')
 
         except Exception as e:
             logger.warning(f'{type(e).__name__}: {e}')
 
 
 if __name__ == '__main__':
-    load_extension()
+    load_commands()
     bot.run(os.getenv('TOKEN'))
