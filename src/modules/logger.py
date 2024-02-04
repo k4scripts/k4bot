b1d0442f7a9a24aa15cf1d35c0cd9486a5a109f8
Bjarne <47014074+iK4oS@users.noreply.github.com>
2024-02-04 23:44:40 +0100

    Suppress other libs logging

--- src/modules/logger.py
+++ src/modules/logger.py
@@ -3,5 +3,9 @@ import logging
 
 LOGGING_LEVEL = int(os.getenv('LOGGING_LEVEL', 2)) * 10
 
+modules = ['discord', 'httpx',]
+for module in modules:
+    logging.getLogger(module).setLevel((LOGGING_LEVEL + 10) if LOGGING_LEVEL < 50 else 50)
+
 logging.basicConfig(level=LOGGING_LEVEL, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 logger = logging.getLogger('k4bot')
